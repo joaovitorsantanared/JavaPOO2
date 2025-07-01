3f5c96a3ffcade72d6360fa7408a8fec7730d529
João Vitor <jvtreloso@gmail.com>
2025-07-01 14:22:27 -0300

    Add files via upload

---
+++ JavaPOO2/src/javapoo2/Cavalo.java
@@ -0,0 +1,20 @@
+package javapoo2;
+
+public class Cavalo {
+	String nome;
+	
+	public Cavalo () {}
+	
+	public Cavalo(String nome) {
+		this.nome=nome;
+	}
+	
+	public void comer() {
+	
+	System.out.println("foi comer");
+	}
+	public void comer(String comida) {
+	 
+		System.out.println("");
+	}
+	
---
+++ JavaPOO2/src/javapoo2/Fazenda.java
@@ -0,0 +1,10 @@
+package javapoo2;
+
+public class Fazenda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
